fd04406f12aad522e06d8e51d4c64ce7824647be
zhonghaoliu <joshualiu94@gmail.com>
2017-11-09 18:55:37 +0000

    finished to step 8: add has_secure_password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,28 @@ class UsersController < ApplicationController
   end
 
   def create
-  end
+    @user = User.new(user_params)
   
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/', notice: 'Account created successfully'
+    else
+      flash[:error] = 'An error occured!'
+      render 'new' 
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :firstName,
+      :lastName,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+
+
 end
