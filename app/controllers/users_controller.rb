9a490f11b310d8437168522fca1783a8c330b96b
zhonghaoliu <joshualiu94@gmail.com>
2017-11-11 23:54:24 +0000

    email address uniqueness validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
+
     @user = User.new(user_params)
-  
-    if @user.save
+
+    if User.find_by_email(user_params[:email])
+      render :signup
+    elsif @user.save  
       session[:user_id] = @user.id
       redirect_to '/', notice: 'Account created successfully'
     else
-      flash[:error] = 'An error occured!'
-      render 'new' 
+      render :signup 
     end
   end
 
